81ec99abff4ddb2890c11e07462b97ef28d3e39c
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2019-01-21 20:34:35 -0500

    Add files via upload

--- 2.3_Stepik.cpp
+++ 2.3_Stepik.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 vector<vector<int>> getInput()
 {
-    string line;    
+    string line;
     getline(cin, line);
 
     string token;
@@ -25,7 +25,7 @@ vector<vector<int>> getInput()
     //get size
     stringstream lineStream(line);
     while(lineStream >> token)
-    {             
+    {
         size++;
     }
 
@@ -40,7 +40,7 @@ vector<vector<int>> getInput()
         stringstream lineStream2(line);
         currSize = 0;
         while(lineStream2 >> token)
-        {       
+        {
 
             test = stoi(token);
             numbers[rowNum][currSize] = test;
@@ -54,7 +54,7 @@ vector<vector<int>> getInput()
     stringstream lineStream2(line);
     currSize = 0;
     while(lineStream2 >> token)
-    {       
+    {
 
         test = stoi(token);
         numbers[rowNum][currSize] = test;
@@ -67,91 +67,113 @@ vector<vector<int>> getInput()
 void solveMaze(vector<vector<int>> maze, int x, int y)
 {
     stack< pair<int,int> > currentLocationStack;
+    stack< pair<int,int> > reverseStack;
+
     bool done = false;
     int currRow = 0;
     int currColumn = 0;
-    
-    //myStack.push(make_pair(myString, myInt)); just used for reference
-    
+
+
     currentLocationStack.push(make_pair(0,0)); //creates the first stack pair at (0,0) which is always the beginning
-    
-    
-    
-    //cout << "Maze size: " << maze.size() << endl;
-    
-    
+
+
+
     while(!done)
     {
         if(currRow == x && currColumn == y)
         {
             done = true;
-            cout << "WIN!" << endl;
-            
-            /*
             while(!currentLocationStack.empty())
             {
-                cout << "(" << currentLocationStack.top().first << ", " << currentLocationStack.top().second << ")";
+                reverseStack.push(currentLocationStack.top());
+                currentLocationStack.pop();
             }
-            //NEED TO FIGURE OUT A WAY TO PRINT THE CORRECT PATH TAKEN PARTICULARY IN REVERSE ORDER
-            */
-            
+
+            while(!reverseStack.empty())
+            {
+                if(reverseStack.size() == 1)
+                {
+                    cout << "(" << reverseStack.top().first << ", " << reverseStack.top().second << ")";
+                }
+                else
+                {
+                    cout << "(" << reverseStack.top().first << ", " << reverseStack.top().second << ") -> ";
+                }
+                reverseStack.pop();
+            }
+
+            break;
+
         }
-        
-        
-        //PROBABLY ACCESING A NEGATE ELEMENT OF MAZE VECTOR SUCH AS maze[-1][0] or something
-        if(((currRow+1) >= 0 && (currRow+1) <= maze.size() && currColumn >= 0 && currColumn <= maze.size()) && (maze[currRow + 1][currColumn] == 1)) //CHECKS SOUTH
+
+
+        if(((currRow+1) >= 0 && (currRow+1) < maze.size() && currColumn >= 0 && currColumn < maze.size())) //CHECKS SOUTH
         {
-            currentLocationStack.push(make_pair(currentLocationStack.top().first + 1, currentLocationStack.top().second)); 
-            currRow = currentLocationStack.top().first;
-            currColumn = currentLocationStack.top().second;
+            if(maze[currRow + 1][currColumn] == 1)
+            {
+                maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED
+                currentLocationStack.push(make_pair(currentLocationStack.top().first + 1, currentLocationStack.top().second));
+                currRow = currentLocationStack.top().first;
+                currColumn = currentLocationStack.top().second;
+                continue;
+            }
         }
-        else if((currRow >= 0 && currRow <= maze.size() && (currColumn+1) >= 0 && (currColumn+1) <= maze.size()) && (maze[currRow][currColumn + 1] == 1)   ) //CHECKS EAST
+
+        if((currRow >= 0 && currRow < maze.size() && (currColumn+1) >= 0 && (currColumn+1) < maze.size()))//CHECKS EAST
         {
-            currentLocationStack.push(make_pair(currentLocationStack.top().first, currentLocationStack.top().second + 1)); 
-            currRow = currentLocationStack.top().first;
-            currColumn = currentLocationStack.top().second;
+            if(maze[currRow][currColumn + 1] == 1)
+            {
+                maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED
+                currentLocationStack.push(make_pair(currentLocationStack.top().first, currentLocationStack.top().second + 1));
+                currRow = currentLocationStack.top().first;
+                currColumn = currentLocationStack.top().second;
+                continue;
+            }
         }
-        else if(((currRow-1) >= 0 && (currRow-1) <= maze.size() && currColumn >= 0 && currColumn <= maze.size()) && (maze[currRow - 1][currColumn] == 1)) //CHECKS NORTH
+
+        if(((currRow-1) >= 0 && (currRow-1) < maze.size()) && (currColumn >= 0 && currColumn < maze.size()))//CHECKS NORTH
         {
-            currentLocationStack.push(make_pair(currentLocationStack.top().first - 1, currentLocationStack.top().second)); 
-            currRow = currentLocationStack.top().first;
-            currColumn = currentLocationStack.top().second;
+            if(maze[currRow - 1][currColumn] == 1)
+            {
+                maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED
+                currentLocationStack.push(make_pair(currentLocationStack.top().first - 1, currentLocationStack.top().second));
+                currRow = currentLocationStack.top().first;
+                currColumn = currentLocationStack.top().second;
+                continue;
+            }
         }
-        else if((currRow >= 0 && currRow <= maze.size() && (currColumn-1) >= 0 && (currColumn-1) <= maze.size()) && maze[currRow][currColumn - 1] == 1) //CHECKS WEST
+        if((currRow >= 0 && currRow < maze.size()) && ((currColumn-1) >= 0 && (currColumn-1) < maze.size()))//CHECKS WEST
         {
-            currentLocationStack.push(make_pair(currentLocationStack.top().first, currentLocationStack.top().second - 1)); 
-            currRow = currentLocationStack.top().first;
-            currColumn = currentLocationStack.top().second;
+            if(maze[currRow][currColumn - 1] == 1)
+            {
+                maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED
+                currentLocationStack.push(make_pair(currentLocationStack.top().first, currentLocationStack.top().second - 1));
+                currRow = currentLocationStack.top().first;
+                currColumn = currentLocationStack.top().second;
+                continue;
+            }
         }
-        else if(!currentLocationStack.empty())//ASSUMES NONE OF THE WAYS ARE VIABLE AND NOT FIRST ENTRY, NEED TO MOVE BACK
-        { 
-            //NEED TO SET LOCATION AS "VISITED"
-            maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED 
-            
-            //NEED TO MOVE BACK TO PREVIOUS SPOT aka pop
-            currentLocationStack.pop();
-        }
-        else
-        {
-            cout << "Something went wrong and no other case evauluated" << endl;    
-        }
-        
-        
-        cout << "Current Row: " << currRow << endl;
-        cout << "Current Column: " << currColumn << endl;
-        cout << endl;
-        
+
+        //NEED TO SET LOCATION AS "VISITED"
+        maze[currentLocationStack.top().first][currentLocationStack.top().second] = 0; //THIS SHOULD MARK IT AS VISITED
+
+        //NEED TO MOVE BACK TO PREVIOUS SPOT aka pop
+        currentLocationStack.pop();
+        currRow = currentLocationStack.top().first;
+        currColumn = currentLocationStack.top().second;
+
+
     }//end while
-    
-    
-    
+
+
+
 } //end solveMaze
 
 int main()
 {
     vector<vector<int>> maze;
     maze = getInput();
-    int x; 
+    int x;
     cin >> x;
     int y;
     cin >> y;
